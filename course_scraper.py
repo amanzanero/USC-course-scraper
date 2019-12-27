9f07d22c68cee7f615e55b587ba1c023b69cb613
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-12-27 11:09:33 -0800

    remove csv functionality and clean up code

--- course_scraper.py
+++ course_scraper.py
@@ -1,3 +1,6 @@
+# Author: Andrew Manzanero
+# Description: Program that scrapes USC's web-page for course data
+
 from bs4 import BeautifulSoup
 import urllib.request
 import urllib.error
@@ -9,47 +12,42 @@ import re
 
 def main():
 
-    # get user info
-    term_input, year_input, jsonOrCsv, customClasses = userInfo()
+    # get user inputs
+    term_input, year_input, user_courses = userInfo()
 
-    # load page
-    fName = filename_generator(term_input, year_input)  # retrieve ID from user
+    # generate a file name and url id
     url_id = "{}{}".format(year_input, term_input)
+    fName = filename_generator(term_input, year_input)
 
-    if jsonOrCsv == 1:
-        fName += '.json'
-    else:
-        fName += '.csv'
-
-    # error handling
+    # url template
     url = "https://classes.usc.edu/term-%s/classes/" % url_id
 
     # retrieve url and grab html for parsing
-
     try:
         page = urllib.request.urlopen(url)
 
     except urllib.error.HTTPError as _:
+        # secondary request for archived courses
         try:
             url = "https://web-app.usc.edu/ws/soc_archive/soc/term-%s/" % url_id
             page = urllib.request.urlopen(url)
         except urllib.error.HTTPError as err:
             if err.code == 404:
-                print("404 Not found. Term is no longer available")
+                print("404 Not found. Term is not available.")
                 return
             else:
                 raise
                 return
 
     soup = BeautifulSoup(page.read(), "html.parser")
-
     school_urls = get_school_tags(soup)
 
-    # dictionary to be json
+    # dictionary if json will be used
     jsonDict = {}
 
-    iterable = customClasses if len(customClasses) > 0 else school_urls.keys()
+    iterable = user_courses if len(user_courses) > 0 else school_urls.keys()
 
+    # iterate over keys and build info store
     for key in iterable:
         if key not in school_urls:
             print("{} is not a valid department code. Continuing...".format(key))
@@ -57,30 +55,19 @@ def main():
         print_string = "Retrieving classes from: %s" % key
         print(print_string)
 
-        csv_list = []
+        course_details_dict = get_course_details(school_urls[key])
+        jsonDict[key] = course_details_dict
 
-        if jsonOrCsv == 2:
-            class_list = print_class_info(school_urls[key])
-            for item in class_list:
-                csv_list.append(item)
-        else:
-            course_details_dict = get_course_details(school_urls[key])
-            jsonDict[key] = course_details_dict
-
-    open_file = None
-    if jsonOrCsv == 1:
-        open_file = save_as_json(jsonDict, fName)
-    else:
-        open_file = save_as_csv(csv_list, fName)
-    open_file.close()
+    # save the file
+    save_as_json(jsonDict, fName)
 
     print('\nScraping success. Goodbye!')
 
 
-def filename_generator(term_input, year_input):
-    """ This creates the term id for the url
-    input: string representing term
-    output: string representing the url id
+def filename_generator(term_input: str, year_input: str) -> str:
+    """Creates a formatted file name
+    input: string representing term, string representing year in YYYY
+    output: string representing file name w/o extension
     """
     term_input = term_input.lower()
     term_string = None
@@ -98,7 +85,7 @@ def filename_generator(term_input, year_input):
     return fName
 
 
-def get_school_tags(soup):
+def get_school_tags(soup: BeautifulSoup) -> dict:
     """ Returns html from each school at USC i.e. dornsife, viterbi
     input: soup
     output: dictionary with school as key and url as value
@@ -114,35 +101,10 @@ def get_school_tags(soup):
     return school_url_dict
 
 
-def print_class_info(url):
-    """ Print our class information
-    input: url with specific USC school
-    output: list with school info
-    """
-    try:
-        page = urllib.request.urlopen(url)
-    except ValueError as _:
-        page = urllib.request.urlopen("https://web-app.usc.edu/{}".format(url))
-    soup = BeautifulSoup(page.read(), "html.parser")
-
-    # get all of the classes
-    class_titles = soup.find_all("div", class_="course-info expandable")
-
-    # searching within each tag
-    return_list = []
-
-    for tag in class_titles:
-        class_name = tag.attrs['id']
-        return_list.append(class_name)
-
-    return return_list
-
-
-def get_course_details(url):
-    """
+def get_course_details(url: str) -> dict:
+    """Gets course details for all courses in the html at that url
     input: url to department course page
-    output: dictionary where keys are course id's and values
-      are dictionaries of details
+    output: list of dictionaries containing code, dept, and title
     """
     try:
         page = urllib.request.urlopen(url)
@@ -155,39 +117,63 @@ def get_course_details(url):
     class_titles = soup.find_all("div", class_="course-info expandable")
 
     return_list = []
-    for tag in class_titles:
-        class_name = tag.attrs['id']
-        course_details_dict = dict()
-        course_details = tag.h3.a.text.strip().split(": ")
-        course_id = tag.h3.a.strong.text.split(":")[0].split(" ")
 
-        course_details_dict["dept"] = course_id[0]
-        course_details_dict["code"] = course_id[1]
-        course_details_dict["title"] = course_details[1]
-        return_list.append(course_details_dict)
+    # scrape the html
+    for tag in class_titles:
+        details = dict()
+
+        # parse course header for title, units, and id
+        course_heading = tag.h3.a.text
+        title_units = course_heading.strip().split(": ")[1]
+        title = title_units.split("(")[0].strip()
+        units = title_units.split("(")[1].split(".")[0]
+        course_id = course_heading.split(":")[0].split(" ")
+
+        # get description
+        description = tag.find("div", class_="catalogue").text
+
+        # try getting prereq and coreq
+        try:
+            prereq = tag.find("li", class_="prereq").text.split(": ")[1]
+            details["prerequisite(s)"] = prereq
+        except AttributeError as err:
+            pass
+
+        try:
+            coreq = tag.find("li", class_="coreq").text.split(": ")[1]
+            details["corequisite(s)"] = coreq
+        except AttributeError as err:
+            pass
+
+        # populate fields
+        details["dept"] = course_id[0]
+        details["code"] = course_id[1]
+        details["title"] = title
+        details["units"] = units
+        if description:
+            details["description"] = description
+
+        return_list.append(details)
 
     return return_list
 
 
-def save_as_csv(class_list, csv_file):
-    """ Print our class information
-    input: save the list of classes as a csv
+def save_as_json(jsonDict: dict, json_file: str) -> None:
+    """ save the dictionary as a json file
+    input: class_list
     output: open file
     """
-    with open(csv_file, "a") as f:
-        wr = csv.writer(f, delimiter=",")
-        wr.writerow(class_list)
-
-    return f
-
-
-def save_as_json(jsonDict, json_file):
     with open(json_file, 'w') as resFile:
         json.dump(jsonDict, resFile, indent=2)
-    return resFile
+    resFile.close()
 
 
-def userInfo():
+def userInfo() -> (str, str, [str]):
+    """Gets 4 user inputs from user 
+    input: None
+    output: term_input, year_input, file_format, user_courses
+    user_courses is the only parameter that could return empty
+    """
     term_input = input("What term would you like to search for?\n"
                        "Please choose from the following:\n"
                        "1) Spring\n"
@@ -201,12 +187,7 @@ def userInfo():
     while not re.match(r'^\d{4}$', year_input):
         year_input = input("(YYYY) >> ")
 
-    jsonOrCsv = input("JSON or CSV? (1) JSON (2) CSV >> ")
-    while not jsonOrCsv.isdigit() and int(jsonOrCsv) != 1 and int(jsonOrCsv) != 2:
-        jsonOrCsv = input("(1/2) >> ")
-    jsonOrCsv = int(jsonOrCsv)
-
-    customClasses = []
+    user_courses = []
     get_classes = input(
         "Would you like to enter a list of courses? Default is all courses. (y/n) >> ")
     while get_classes.lower() not in ["y", "n"]:
@@ -216,9 +197,9 @@ def userInfo():
         classes = input(
             "Enter a comma separated list of department codes (i.e. MATH, AME)\n>> ")
         classes = classes.split(",")
-        customClasses = [item.strip().upper() for item in classes]
+        user_courses = [item.strip().upper() for item in classes]
 
-    return term_input, year_input, jsonOrCsv, customClasses
+    return term_input, year_input, user_courses
 
 
 if __name__ == '__main__':
